a1d6baf0a1b1fbb16d3b7f7e55b16b8cf15eb3b8
Gregory Daynes <greg.daynes@pixelunion.net>
2020-03-16 19:18:49 -0700

    Implement Auth, Next, Polaris Components

---
+++ src/pages/_app.js
@@ -0,0 +1,36 @@
+import App from "next/app";
+import Head from "next/head";
+import { AppProvider } from "@shopify/polaris";
+import { Provider } from "@shopify/app-bridge-react";
+import "@shopify/polaris/styles.css";
+import translations from "@shopify/polaris/locales/en.json";
+import Cookies from "js-cookie";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    const config = {
+      // apiKey: API_KEY // TODO determine how to get this resolved
+      apiKey: "4fe42b35e76b863d1b1d6698c7b7b29c",
+      shopOrigin: Cookies.get("shopOrigin"),
+      forceRedirect: true
+    };
+
+    return (
+      <>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+
+        <Provider config={config}>
+          <AppProvider i18n={translations}>
+            <Component {...pageProps} />
+          </AppProvider>
+        </Provider>
+      </>
+    );
+  }
+}
+
+export default MyApp;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-require('isomorphic-fetch');
+require("isomorphic-fetch");
 
-const dotenv = require('dotenv');
-const Koa = require('koa');
-const next = require('next');
-const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
-const { verifyRequest } = require('@shopify/koa-shopify-auth');
-const session = require('koa-session');
+const dotenv = require("dotenv");
+const Koa = require("koa");
+const next = require("next");
+const { default: createShopifyAuth } = require("@shopify/koa-shopify-auth");
+const { verifyRequest } = require("@shopify/koa-shopify-auth");
+const session = require("koa-session");
 
 dotenv.config();
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -18,28 +18,34 @@ const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
 app.prepare().then(() => {
   const server = new Koa();
-  server.use(session({ secure: true, sameSite: 'none' }, server));
+  server.use(session({ secure: true, sameSite: "none" }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
 
   server.use(
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['read_products'],
+      scopes: ["read_products"],
       afterAuth(ctx) {
-        const { shop, accessToken } = ctx.session;
+        const { shop } = ctx.session;
 
-        ctx.redirect('/');
+        ctx.cookies.set("shopOrigin", shop, {
+          httpOnly: false,
+          secure: true,
+          sameSite: "none",
+        });
+
+        ctx.redirect("/");
       },
-    }),
+    })
   );
 
   server.use(verifyRequest());
-  server.use(async (ctx) => {
+  server.use(async ctx => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
-    return
+    return;
   });
 
   server.listen(port, () => {
